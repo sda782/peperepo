1c15e579ab383db4fd95ab8f1a481d7c6057a038
sda782 <saerdna.petersen@gmail.com>
2023-03-14 00:16:13 +0100

    paste image

---
+++ src/lib/Header/AddModal.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+    import { Center, Modal, Image, Space, Button } from "@svelteuidev/core";
+    import { upload } from "../firebaseStorage/firebaseStorageApi";
+    import addButonImage from "../../assets/add_white.png";
+
+    export let opened: boolean = false;
+    let preview: string = undefined;
+    let addbutton: HTMLInputElement;
+
+    const onFileSelected = (e) => {
+        let image = e.target.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onload = (e: ProgressEvent<FileReader>) => {
+            preview = e.target.result.toString();
+        };
+    };
+
+    const onClose = () => {
+        opened = false;
+        preview = undefined;
+    };
+
+    const Upload = () => {
+        if (preview === undefined) return;
+        upload(preview);
+        onClose();
+    };
+
+    async function pasteImage() {
+        try {
+            const permissionName = "clipboard-read" as PermissionName;
+            const permission = await navigator.permissions.query({
+                name: permissionName,
+            });
+            if (permission.state === "denied") {
+                throw new Error("Not allowed to read clipboard.");
+            }
+            const clipboardContents = await navigator.clipboard.read();
+            for (const item of clipboardContents) {
+                if (!item.types.includes("image/png")) {
+                    addbutton.click();
+                    throw new Error("Clipboard contains non-image data.");
+                }
+                const blob = await item.getType("image/png");
+                preview = URL.createObjectURL(blob);
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+</script>
+
+<Modal centered {opened} on:close={onClose} title="Upload rare pepe" size="xs">
+    <Center>
+        {#if preview}
+            <Image src={preview} />
+        {:else}
+            <Image
+                width="150px"
+                src={addButonImage}
+                on:click={() => {
+                    pasteImage();
+                }} />
+        {/if}
+    </Center>
+    <input
+        style="display:none"
+        type="file"
+        accept=".jpg, .jpeg, .png"
+        alt="image input"
+        on:change={(e) => onFileSelected(e)}
+        bind:this={addbutton} />
+    <Space h="lg" />
+    <Button fullSize on:click={Upload}>Upload</Button>
+</Modal>
--- src/lib/Header/AddNewImage.svelte
+++ src/lib/Header/AddNewImage.svelte
@@ -1,55 +1,11 @@
 <script lang="ts">
-    import { Button, Modal, Image, Space, Center } from "@svelteuidev/core";
-    import { upload } from "../firebaseStorage/firebaseStorageApi";
-    import addButonImage from "../../assets/add_white.png";
+    import { Button } from "@svelteuidev/core";
 
-    let opened: boolean = false;
-    let preview: string = undefined;
-    let addbutton: HTMLInputElement;
+    import AddModal from "./AddModal.svelte";
 
-    const onFileSelected = (e) => {
-        let image = e.target.files[0];
-        let reader = new FileReader();
-        reader.readAsDataURL(image);
-        reader.onload = (e: ProgressEvent<FileReader>) => {
-            preview = e.target.result.toString();
-        };
-    };
-
-    const onClose = () => {
-        opened = false;
-        preview = undefined;
-    };
-
-    const Upload = () => {
-        if (preview === undefined) return;
-        upload(preview);
-        onClose();
-    };
+    let opened: boolean;
 </script>
 
 <Button size="xs" variant="outline" on:click={() => (opened = true)}
     >Upload new Pepe</Button>
-<Modal centered {opened} on:close={onClose} title="Upload rare pepe" size="xs">
-    <Center>
-        {#if preview}
-            <Image src={preview} />
-        {:else}
-            <Image
-                width="150px"
-                src={addButonImage}
-                on:click={() => {
-                    addbutton.click();
-                }} />
-        {/if}
-    </Center>
-    <input
-        style="display:none"
-        type="file"
-        accept=".jpg, .jpeg, .png"
-        alt="image input"
-        on:change={(e) => onFileSelected(e)}
-        bind:this={addbutton} />
-    <Space h="lg" />
-    <Button fullSize on:click={Upload}>Upload</Button>
-</Modal>
+<AddModal bind:opened />
